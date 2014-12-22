55b8653d080222fd201010b7b553df8fb9a3799f
zhang.hc <zhang.hc@yougou.com>
2014-12-22 14:28:00 +0800

    操作失误

--- src/main/java/com/ydp/godview/dao/SysLogDao.java
+++
@@ -1,5 +0,0 @@
-package com.ydp.godview.dao;
-
-public interface SysLogDao {
-	
-}
--- target/classes/com/ydp/godview/model/sys_log.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `sys_log` (
-  `id` varchar(32) NOT NULL,
-  `req_method` varchar(64) NOT NULL DEFAULT '' COMMENT '被请求的方法',
-  `req_param` varchar(64) NOT NULL DEFAULT '' COMMENT '请求的参数',
-  `req_url` varchar(512) NOT NULL DEFAULT '' COMMENT '请求的url',
-  `resp_data` LONGBLOB COMMENT '返回的数据',
-  `create_time` datetime COMMENT '创建的时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='动态日志记录';
